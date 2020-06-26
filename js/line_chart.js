160c7ed16408a17f9791171381c10bf8253602ac
Jonathan Giguere <jonathangiguere@gwu.edu>
2020-06-25 20:13:55 -0400

    Secured login to make public

---
+++ js/line_chart.js
@@ -0,0 +1,54 @@
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#line_chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("https://raw.githubusercontent.com/jonathangiguere/Robinhood_Investment_Web_App/master/data/AAPL.csv?token=AM5LXBJUO7WS65OQGLECKBC66U6XC",
+
+    // When reading the csv, I must format variables:
+    function(d){
+    return { begins_at : d3.timeParse("%Y-%m-%d")(d.begins_at), close_price : +d.close_price,
+            volume : +d.volume, symbol : d.symbol }
+    },
+
+    // Now I can use this dataset:
+    function(data) {
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.begins_at; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.volume; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.begins_at) })
+        .y(function(d) { return y(d.volume) })
+        )
+
+});
+
+
